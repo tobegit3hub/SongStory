68e726ddac154810693772e72afa131578b33f76
tobe <tobe@tobe.(none)>
2013-07-28 22:26:20 +0800

    add license and  readme

--- SongStoryCreateSs/SongStoryCreateSs.py
+++ SongStoryCreateSs/SongStoryCreateSs.py
@@ -5,30 +5,31 @@ import sys
 import os
 import zipfile
 
-number = sys.argv[1]
+def main:
+    number = sys.argv[1]
 
+    songFileName = number+".mp3"
+    lyricsFileName = number+".lrc"
+    albumImageFileName = number+"_album.jpg"
+    imageFileName0 = number+"_0.jpg"
+    imageFileName1 = number+"_1.jpg"
+    imageFileName2 = number+"_2.jpg"
+    jsonFileName = number+".json"
+    
+    os.rename("model.json", jsonFileName)
+    
+    archiveFileNames = [songFileName, lyricsFileName, albumImageFileName, imageFileName0, imageFileName1, imageFileName2, jsonFileName]
+    
+    #print archiveFileNames
+    
+    outputFileName = number+".ss"
+    zout = zipfile.ZipFile(outputFileName, "w")
+    
+    for fileName in archiveFileNames:
+        zout.write(fileName)
+    zout.close()
 
-songFileName = number+".mp3"
-lyricsFileName = number+".lrc"
-albumImageFileName = number+"_album.jpg"
-imageFileName0 = number+"_0.jpg"
-imageFileName1 = number+"_1.jpg"
-imageFileName2 = number+"_2.jpg"
-jsonFileName = number+".json"
-
-os.rename("model.json", jsonFileName)
-
-
-archiveFileNames = [songFileName, lyricsFileName, albumImageFileName, imageFileName0, imageFileName1, imageFileName2, jsonFileName]
-
-#print archiveFileNames
-
-outputFileName = number+".ss"
-zout = zipfile.ZipFile(outputFileName, "w")
-
-for fileName in archiveFileNames:
-    zout.write(fileName)
-zout.close()
-
+if __name__ == "__main__":
+    main()
 
 
--- SongStoryWriteJson/SongStoryWriteJson.py
+++ SongStoryWriteJson/SongStoryWriteJson.py
@@ -3,28 +3,28 @@
 
 import json
 
+def main():
+    storyFile1 = open("1.txt", "r")
+    storyFile2 = open("2.txt", "r")
+    storyFile3 = open("3.txt", "r")
+    jsonFile = open("model.json", "r+")
+    
+    jsonObject = json.load(jsonFile)
+    jsonObject["story_content_1"] = storyFile1.read().replace('"','\"')
+    jsonObject["story_content_2"] = storyFile2.read().replace('"','\"')
+    jsonObject["story_content_3"] = storyFile3.read().replace('"','\"')
+    
+    newJson = json.dumps(jsonObject)
+    
+    jsonFile.close()
+    storyFile1.close()
+    storyFile2.close()
+    storyFile3.close()
+    
+    resultFile = open("model.json", "w+")
+    
+    resultFile.write(newJson)
+    resultFile.close()
 
-storyFile1 = open("1.txt", "r")
-storyFile2 = open("2.txt", "r")
-storyFile3 = open("3.txt", "r")
-jsonFile = open("model.json", "r+")
-
-
-jsonObject = json.load(jsonFile)
-jsonObject["story_content_1"] = storyFile1.read().replace('"','\"')
-jsonObject["story_content_2"] = storyFile2.read().replace('"','\"')
-jsonObject["story_content_3"] = storyFile3.read().replace('"','\"')
-
-newJson = json.dumps(jsonObject)
-
-jsonFile.close()
-storyFile1.close()
-storyFile2.close()
-storyFile3.close()
-
-
-resultFile = open("model.json", "w+")
-
-resultFile.write(newJson)
-resultFile.close()
-
+if __name__ == "__main__":
+    main()
